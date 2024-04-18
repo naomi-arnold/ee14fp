faeb56942171306212928393b9cad0f66ee4fc00
naomi-arnold <narnol01@vm-hw04.eecs.tufts.edu>
2024-04-18 13:55:45 -0400

    “message”

---
+++ RTE/_Target_1/RTE_Components.h
@@ -0,0 +1,17 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'pain' 
+ * Target:  'Target 1' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+/* Keil::Device:STM32Cube Framework:STM32CubeMX:1.1.0 */
+#define RTE_DEVICE_FRAMEWORK_CUBE_MX
+
+
+#endif /* RTE_COMPONENTS_H */
